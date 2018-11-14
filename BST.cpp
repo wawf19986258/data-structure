a741dce8790fa4ad461393b0f31032a1509488c6
wawf19986258 <44578432+wawf19986258@users.noreply.github.com>
2018-11-14 23:46:38 +0800

    Update BST.cpp

--- BST.cpp
+++ BST.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<queue>
 using namespace std;
 
 struct BstNode{
@@ -89,6 +90,39 @@ int findHeight(BstNode* root){
     return max(findHeight(root->left), findHeight(root->right))+1;
 }
 
+void levelOrder(BstNode* root){
+    queue<BstNode*> Q;
+    Q.push(root);
+    while(!Q.empty()){
+        BstNode* current = Q.front();
+        cout << current->data << " ";
+        if(current->left != NULL) Q.push(current->left);
+        if(current->right != NULL) Q.push(current->right);
+        Q.pop();
+    }
+}
+
+void preOrder(BstNode* root){
+    if(root == NULL) return;
+    cout << root->data << " ";
+    preOrder(root->left);
+    preOrder(root->right);
+}
+
+void inOrder(BstNode* root){
+    if(root == NULL) return;
+    inOrder(root->left);
+    cout << root->data << " ";
+    inOrder(root->right);
+}
+
+void postOrder(BstNode* root){
+    if(root == NULL) return;
+    postOrder(root->left);
+    postOrder(root->right);
+    cout << root->data << " ";
+}
+
 int main(){
     BstNode* root = NULL;
     root = insert(root, 7);
@@ -96,7 +130,14 @@ int main(){
     root = insert(root, 8);
     root = insert(root, 10);
     root = insert(root, 3);
-    cout << findHeight(root) << endl;
+    levelOrder(root);
+    cout << endl;
+    preOrder(root);
+    cout << endl;
+    inOrder(root);
+    cout << endl;
+    postOrder(root);
+    cout << endl;
     return 0;
 }
 
